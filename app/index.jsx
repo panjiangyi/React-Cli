d3358ca61b7d9fe7fbac1f645f9185a97d13da32
pjy <78237724@qq.com>
2018-03-15 16:25:16 +0800

    update

--- app/index.jsx
+++ app/index.jsx
@@ -11,15 +11,30 @@ import promiseMiddleware from 'redux-promise';
 import reducer from './store/reducers/reducer.js';
 import {
     createAction,
-    handleAction
+    createActions,
+    handleActions,
+    handleAction,
+    combineActions
 } from 'redux-actions';
 
 const { dispatch, getState } = createStore(reducer, applyMiddleware(promiseMiddleware));
-dispatch(Promise.resolve({
-    type: 'ADD',
-    count: 12
-}))
-    .then(e => dispatch(e))
 const state = getState();
-const increment = createAction('INCREMENT');
-console.log(increment('hao'));
+const {a,b} = createActions({
+    a:e=>'a',
+    b:[e=>'payload',e=>'meta'],
+    c:{
+        d:d=>'d'
+    }
+}, 'ACTION_THREE');
+var c = handleAction(combineActions(a,b),(s,aa)=>s,0)
+console.log(c(0,a()))
+
+
+const r = handleActions({
+        jia(s){
+            return s+1 
+        },
+        jian(s){
+            return s-1
+        }
+},{wa:0})
